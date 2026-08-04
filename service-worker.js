79adb9b9768dc563590907d14fca349cfa17a5bb
jackjones0056-design <jackjones.0056@gmail.com>
2026-08-04 14:22:18 -0500

    Cache anonymous authentication build

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = "meps-coordinator-ios-v4";
+const CACHE_NAME = "meps-coordinator-ios-v5";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css",
-  "./ios-overrides.css?v=ios-20260804-4",
+  "./ios-overrides.css?v=ios-20260804-5",
   "./app.js",
-  "./app-core.js?v=ios-20260804-4",
-  "./app-sync.js?v=ios-20260804-4",
-  "./app-main.js?v=ios-20260804-4",
-  "./auth-password.js?v=ios-20260804-4",
+  "./app-core.js?v=ios-20260804-5",
+  "./app-sync.js?v=ios-20260804-5",
+  "./auth-anonymous.js?v=ios-20260804-5",
+  "./app-main.js?v=ios-20260804-5",
   "./manifest.webmanifest",
   "./icons/icon.svg"
 ];
